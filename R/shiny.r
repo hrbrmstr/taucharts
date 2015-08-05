8e4bcef9cc02b68894ab701d2c40ac21c628852e
Bob Rudis <bob@rudis.net>
2015-08-05 06:58:10 -0400

    Example Shiny apps

--- R/shiny.r
+++ R/shiny.r
@@ -1,3 +1,60 @@
+#' Run a built-in example Shiny app
+#'
+#' When run without specifying \code{app_name}, this function will return
+#' a list of built-in TauCharts Shiny example applications. To run one of the
+#' built-in example applications, specify only the name in the \code{app_name}
+#' parameter and the function will display the full path to the app (making it
+#' easier to find and view the source) and then execute the app.
+#'
+#' @param app_name name of built-in Shiny app to run (if \code{NULL} displays a list
+#'        of built-in apps)
+#' @export
+#' @examples \dontrun{
+#' # list the built-in apps
+#'
+#' run_tau_app()
+#' Available shiny example apps:
+#' -----------------------------
+#' tau01 - TauCharts Shiny Example 01
+#'
+#' # run an app
+#'
+#' run_tau_app("tau01")
+#' Running shiny app [tau01] from:
+#  /Library/Frameworks/R.framework/Versions/3.2/Resources/library/taucharts/shinyapps/tau01
+#  Listening on http://127.0.0.1:6077
+#' }
+run_tau_app <- function(app_name=NULL) {
+
+  pkg_dir <- system.file(package="taucharts")
+
+  if (is.null(app_name)) {
+
+    cat("Available shiny example apps:\n")
+    cat("-----------------------------\n")
+
+    app_files <- list.files(pkg_dir, "ui.R", recursive=TRUE)
+    invisible(sapply(app_files, function(x) {
+
+      cat(gsub("^# META: ", "",
+           grep("^# META:", readLines(paste0(pkg_dir, "/", x)), value=TRUE)))
+      cat("\n")
+
+    }))
+
+  } else {
+
+    app_dir <- sprintf("%s/shinyapps/%s", pkg_dir, app_name)
+    if (dir.exists(app_dir)) {
+      message(paste0("Running shiny app [", app_name, "] from:\n  ", app_dir))
+      shiny::runApp(app_dir)
+    } else {
+      message("Shiny example app not found.\nExecute run_tau_app() without parameters to see available shiny apps.")
+    }
+  }
+
+}
+
 #' Shiny bindings for taucharts
 #'
 #' Output and render functions for using taucharts within Shiny
--- README.md
+++ README.md
@@ -29,6 +29,7 @@ The following functions are implemented:
 -   `tau_legend`: Add a TauCharts tooltip
 -   `tau_tooltip`: Add a TauCharts tooltip
 -   `tau_trendline`: Add a TauCharts trendline
+-   `run_tau_app`: Run a built-in example Shiny app
 
 The following datasets are included:
 
@@ -36,7 +37,8 @@ The following datasets are included:
 
 ### News
 
--   Version 0.2.0.9000 released : stacked bar charts
+-   Version 0.2.5.9000 released : list & run example Shiny apps
+-   Version 0.2.5.9000 released : stacked bar charts
 -   Version 0.1.0 released : trendline, tooltips & dev fork (prod is pretty much stable & functional)
 -   Version 0.0.1.9003 released : facet-based ordering + legends
 -   Version 0.0.1.9000 released : auto-detects column classes, can add manual colors & faceted plots are now working (see the Rpub for an example)
@@ -55,7 +57,7 @@ library(taucharts)
 
 # current verison
 packageVersion("taucharts")
-#> [1] '0.2.0.9001'
+#> [1] '0.2.5.9000'
 ```
 
 ### Test Results
@@ -65,7 +67,7 @@ library(taucharts)
 library(testthat)
 
 date()
-#> [1] "Tue Aug  4 21:21:36 2015"
+#> [1] "Wed Aug  5 06:57:23 2015"
 
 test_dir("tests/")
 #> testthat results ========================================================================================================
