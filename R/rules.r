81b6b07f9fb1251f6489937d4eb5f22a2aed7c0b
Bob Rudis <bob@rudis.net>
2015-08-12 16:37:44 -0400

    CSS warning

--- R/rules.r
+++ R/rules.r
@@ -12,9 +12,11 @@
 #'
 #' @param tau taucharts object
 #' @param rule character vector of CSS rule(s) to add to the widget DOM
+#' @param warn show warnings for global CSS rules? (default: \code{TRUE})
 #' @return taucharts htmlwidget object
 #' @note This is for expert use only. You need to know quite a bit about the visualization
-#'       and target DOM to effectively use this function.
+#'       and target DOM to effectively use this function. CSS rules without the \code{\{\{ID\}\}}
+#'       are applied to the entire DOM.
 #' @export
 #' @examples
 #' # change the default white tooltip to black
@@ -33,7 +35,18 @@
 #'   tau_color_manual(c("blue", "maroon", "black")) %>%
 #'   tau_tooltip() %>%
 #'   make_black_tooltip()
-tau_add_css_rule <- function(tau, rule) {
+tau_add_css_rule <- function(tau, rule, warn=TRUE) {
+
+  # if any of the CSS statements in 'rule' do not have {{ID}} targets, warn the user
+  if (warn) {
+    if (!any(grepl("\\{\\{ID\\}\\}", rule))) {
+      # special case for the tooltip since that resides outsie the widget div
+      # it has to be targeted globally unless the TauCharts folks change the behavior
+      if (!all(grepl("graphical-report__tooltip", rule[which(!grepl("\\{\\{ID\\}\\}", rule))]))) {
+        message("NOTE: CSS rules without {{ID}} are applied to the entire DOM.")
+      }
+    }
+  }
   tau$x$forCSS <- c(tau$x$forCSS, rule)
   tau
 }
